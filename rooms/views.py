c7efbc181da3ed08ed3123a5995f4730ace6fe67
jaey p <jp.inseoul@gmail.com>
2019-10-30 13:20:59 -0400

    added comments for limiting QeurySets

--- rooms/views.py
+++ rooms/views.py
@@ -5,6 +5,9 @@ from . import models
 
 
 def all_rooms(request):
-    all_rooms = models.Room.objects.all()
+    """ Limiting QuerySets
+        Use a subset of Python’s array-slicing syntax to limit your QuerySet to a certain number of results.
+        This is the equivalent of SQL’s LIMIT and OFFSET clauses.
+    """
+    all_rooms = models.Room.objects.all()[:10]
     return render(request, "rooms/home.html", context={"rooms": all_rooms})
-
