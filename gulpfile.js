3ec84d4492b71cd27f26d4c2fb19bd7cfc57bb06
jaey p <jp.inseoul@gmail.com>
2019-11-16 16:37:25 -0500

    added img bundling to npm run build

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,51 @@
 const gulp = require("gulp");
+const image = require("gulp-image");
+const postCSS = require("gulp-postcss");
+const sass = require("gulp-sass");
+const minify = require("gulp-csso");
+const del = require("del");
 
-const css = () => {
-  const postCSS = require("gulp-postcss");
-  const sass = require("gulp-sass");
-  const minify = require("gulp-csso");
-  sass.compiler = require("node-sass");
-  return gulp
+/**
+ * Routing Object
+ */
+const routes = {
+  img: {
+    src: "assets/img/*",
+    dest: "static/img"
+  },
+  scss: {
+    src: "assets/scss/style.css",
+    dest: "static/css"
+  }
+};
+
+/**
+ * Task Methods
+ */
+const taskImg = () =>
+  gulp
+    .src(routes.img.src)
+    .pipe(image())
+    .pipe(gulp.dest(routes.img.dest));
+
+const taskStyle = () =>
+  gulp
     .src("assets/scss/style.scss")
     .pipe(sass().on("error", sass.logError))
     .pipe(postCSS([require("tailwindcss"), require("autoprefixer")]))
     .pipe(minify())
     .pipe(gulp.dest("static/css"));
-};
 
-exports.default = css;
+const clean = () => del(["static"]);
+
+/**
+ * Series/Parallel Methods
+ */
+const prepare = gulp.series([clean]);
+const jobs = gulp.series([taskImg, taskStyle]);
+
+/**
+ * Exports Methods
+ */
+exports.clean = gulp.series([clean]);
+exports.default = gulp.series([prepare, jobs]); // npm run build
