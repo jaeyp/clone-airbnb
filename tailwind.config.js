98ef85dcae9923b347d1e99a6e5b5c12b49a4996
jaey p <jp.inseoul@gmail.com>
2019-11-23 16:31:20 -0500

    added custom styles h-n0vh and bg-zoom class + responsive hover

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,11 @@ module.exports = {
       spacing: {
         // "name of the class": "CSS parameter"
         "25vh": "25vh",
+        "30vh": "30vh",
+        "40vh": "40vh",
         "50vh": "50vh",
+        "60vh": "60vh",
+        "70vh": "70vh",
         "75vh": "75vh"
       },
       borderRadius: {
@@ -15,9 +19,17 @@ module.exports = {
       },
       borderWidth: {
         "16": "16px"
+      },
+      backgroundSize: {
+        zoom: "120%" // bg-zoom
       }
     }
   },
-  variants: {},
+  variants: {
+    // https://tailwindcss.com/docs/background-size#customizing
+    // apply bg-zoom with hover event.
+    // e.g. class="hover:bg-zoom"
+    backgroundSize: ["responsive", "hover"]
+  },
   plugins: []
 };
