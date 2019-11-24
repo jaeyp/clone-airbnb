3e1b5d3ddb1e2b67b0987588b30fa5222daae08e
jaey p <jp.inseoul@gmail.com>
2019-11-24 00:48:57 -0500

    added comments for review date

--- reviews/models.py
+++ reviews/models.py
@@ -17,6 +17,8 @@ class Review(AbsctractTimeStampedModel):
     value = models.IntegerField()
     user = models.ForeignKey("users.User", related_name="reviews", on_delete=models.CASCADE)
     room = models.ForeignKey("rooms.Room", related_name="reviews", on_delete=models.CASCADE)
+    # TODO: adding review date => use 'created'
+    # AbsctractTimeStampedModel has 'created' attribute.
 
     def __str__(self):
         return f"{self.review} - {self.room}"
@@ -26,14 +28,7 @@ class Review(AbsctractTimeStampedModel):
     """
 
     def rating_average(self):
-        avg = (
-            self.accuracy
-            + self.communication
-            + self.cleanliness
-            + self.location
-            + self.check_in
-            + self.value
-        ) / 6
+        avg = (self.accuracy + self.communication + self.cleanliness + self.location + self.check_in + self.value) / 6
         return round(avg, 2)
 
     rating_average.short_description = "Avg."
