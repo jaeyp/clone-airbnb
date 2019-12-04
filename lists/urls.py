ee7f9ba5ac4b03e6e515b3d52c357b7b91ed1629
jaey p <jp.inseoul@gmail.com>
2019-12-04 14:42:11 -0500

    added comments

--- lists/urls.py
+++ lists/urls.py
@@ -4,7 +4,9 @@ from . import views
 app_name = "lists"
 
 urlpatterns = [
+    # {% url 'lists:toggle-room' room.pk %}
     path("toggle/<int:room_pk>", views.toggle_room, name="toggle-room"),
+    # {% url 'lists:show-favs' %}
     # path("favs/", views.show_favs, name="show-favs"),
     path("favs/", views.ShowFavsView.as_view(), name="show-favs"),
 ]
