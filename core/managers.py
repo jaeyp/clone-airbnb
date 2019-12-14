8d7e9182e53b9e794752b6b82a69d7f68b1ea300
jaey p <jp.inseoul@gmail.com>
2019-12-14 15:35:06 -0500

    EB Deployment: fixed no create_superuser() issue

--- core/managers.py
+++ core/managers.py
@@ -1,7 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import UserManager
 
 
-class CustomModelManager(models.Manager):
+class CustomModelManager(models.Manager):  # for other models except for User model
     """ Custom managers - extending default manager (models.Manager)
         https://docs.djangoproject.com/en/2.2/topics/db/managers/#custom-managers
     """
@@ -11,3 +12,8 @@ class CustomModelManager(models.Manager):
             return self.get(**kwargs)
         except self.model.DoesNotExist:
             return None
+
+
+class CustomUserManager(CustomModelManager, UserManager):  # for User model in order to support create_superuser()
+    pass
+
--- users/models.py
+++ users/models.py
@@ -91,7 +91,8 @@ class User(AbstractUser):
     """ TODO: bug-fix: creating a new user occurs error ('CustomModelManager' object has no attribute 'normalize_email')
     objects = core_managers.CustomModelManager()
     """
-    objects = core_managers.CustomModelManager()
+    #  objects = core_managers.CustomModelManager()
+    objects = core_managers.CustomUserManager()  # Support create_superuser() for EB deployment
 
     def get_absolute_url(self):
         # print(f"users:profile with {self.pk}")
