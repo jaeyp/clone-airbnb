59d4c1eb445f1149d16b90bc3826e64413e8b4f1
jaey p <jp.inseoul@gmail.com>
2019-12-14 19:39:13 -0500

    EB Deployment: fixed 'The STATIC_URL setting must end with a slash' error

--- config/settings.py
+++ config/settings.py
@@ -263,7 +263,7 @@ if not DEBUG:
     AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
 
     # override static and media file path
-    STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static"
+    STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static/"
 
     # Sentry
     sentry_sdk.init(dsn=os.environ.get("SENTRY_URL"), integrations=[DjangoIntegration()], send_default_pii=True)
